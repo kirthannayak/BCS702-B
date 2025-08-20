186b1419c51ec8cb83270a0d7a3c5ab62bc61f6f
kirthannayak <160410333+kirthannayak@users.noreply.github.com>
2025-08-20 15:02:21 +0530

    Update cie1.c

--- cie1.c
+++ cie1.c
@@ -2,50 +2,66 @@
 #include <stdlib.h>
 #include <time.h>
 
-void bubbleSort(int arr[], int n) {
-    int i, j, temp;
-    for (i = 0; i < n-1; i++) {
-        // Last i elements are already in place
-        for (j = 0; j < n-i-1; j++) {
-            if (arr[j] > arr[j+1]) {
-                // Swap arr[j] and arr[j+1]
-                temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
-            }
-        }
+void merge(int arr[], int left, int mid, int right) {
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    int *L = malloc(n1 * sizeof(int));
+    int *R = malloc(n2 * sizeof(int));
+
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[left + i];
+    for (int j = 0; j < n2; j++)
+        R[j] = arr[mid + 1 + j];
+
+    int i = 0, j = 0, k = left;
+
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j])
+            arr[k++] = L[i++];
+        else
+            arr[k++] = R[j++];
+    }
+
+    while (i < n1)
+        arr[k++] = L[i++];
+    while (j < n2)
+        arr[k++] = R[j++];
+
+    free(L);
+    free(R);
+}
+
+void mergeSort(int arr[], int left, int right) {
+    if (left < right) {
+        int mid = left + (right - left) / 2;
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+        merge(arr, left, mid, right);
     }
 }
 
 int main() {
-    int n = 1000; // Number of random 4-digit numbers
+    int n = 10000;  // Number of random 4-digit numbers
     int *arr = malloc(n * sizeof(int));
-    if (arr == NULL) {
+    if (!arr) {
         printf("Memory allocation failed\n");
         return 1;
     }
 
     srand(time(NULL));
-
-    // Generate random 4-digit numbers (1000 to 9999)
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
         arr[i] = rand() % 9000 + 1000;
-    }
 
-    // Record start time
     clock_t start = clock();
 
-    // Bubble sort
-    bubbleSort(arr, n);
+    mergeSort(arr, 0, n - 1);
 
-    // Record end time
     clock_t end = clock();
 
-    // Calculate elapsed time in seconds
-    double time_spent = (double)(end - start) / CLOCKS_PER_SEC;
+    double time_taken = ((double)(end - start)) / CLOCKS_PER_SEC;
 
-
-    printf("Bubble sort of %d random 4-digit numbers took %f seconds.\n", n, time_spent);
+    printf("Merge sort of %d random 4-digit numbers took %f seconds.\n", n, time_taken);
 
     free(arr);
     return 0;
