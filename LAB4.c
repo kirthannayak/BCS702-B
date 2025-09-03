402e8bae5cbf3fba40d82b6e1ade1bbc564dfb88
kirthannayak <160410333+kirthannayak@users.noreply.github.com>
2025-09-03 15:33:45 +0530

    Update LAB4.c

--- LAB4.c
+++ LAB4.c
@@ -1,15 +1,21 @@
 #include <stdlib.h>
-#include<omp.h>
+#include <omp.h>
 #include <math.h>
 #include <stdio.h>
 
 int is_prime(int num) {
     if (num <= 1) return 0;
-    if (num == 2) return 1;
+    if (num == 2)
+    {
+        printf("%d ",num);
+     return 1;
+    }
     if (num % 2 == 0) return 0;
     for (int i = 3; i <= sqrt(num); i += 2) {
         if (num % i == 0) return 0;
     }
+    
+        printf("%d ",num);
     return 1;
 }
 
